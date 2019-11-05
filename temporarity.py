c45f1f704c7ef4d99da14f6fbd5a534175e34d32
Jacob Folsom <cercops@gmail.com>
2019-11-04 19:57:31 -0500

    Before first chapter project

--- temporarity.py
+++ temporarity.py
@@ -1,18 +1,2 @@
-# Print Unicode Chart
-firstuni = 32    #first character to print
-lastuni = 112    #last character to print
-perline = 10    #number of characters per line
-
-
-for i in range(lastuni - firstuni):
-    hposition = i % perline
-    ipair = str(firstuni + i) + chr(firstuni + i)
-    ipair = ipair.ljust(4)
-    print(ipair, end='|')
-    # start a new line once perline pairs have printed
-    ## print('i', i)
-    ## print('hposition', hposition)
-    if (hposition+1) % perline == 0:
-        print('\n')
-
-
+import pyperclip
+print(pyperclip.paste())
