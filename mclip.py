742debe9d3b342ea6b08d8e373acc8041e511ab3
jfolsom <cercops@gmail.com>
2019-11-25 10:17:49 -0500

    merge home into work comp

--- mclip.py
+++ mclip.py
@@ -1,12 +1,33 @@
 #! python python3
 # mclip.py - A multi-clipboard program.
+import sys
+import pyperclip
+
+#create variables with empty strings in case they are needed later
+senddata = ''
+low28 = ''
+funkybreak = ''
+
+if sys.argv[1] == 'senddata':
+    senddata = (sys.argv[2] + """,\n\nWe've completed testing for """ + sys.argv[3] +
+               """.  Kindly confirm receipt, and if you have any question or concerns, let me know those to.\n""" +
+               """Thanks for the work and best wishes.""")
+if sys.argv[1] == 'low28':
+    low28 = ("""I'm signing a break report with low 28 day breaks for set """ + sys.argv[2] +
+             """ from """ + sys.argv[4] + """.""")
+
+if sys.argv[1] == 'funkybreak':
+    funkybreak = ("""I'm signing a concrete report for set """ + sys.argv[2] +
+                  """I'm flagging this set because of: """ + sys.argv[3])
+
 
 TEXT = {'agree': """Yes, I agree.  That sounds fine to me.""",
         'busy': """Sorry, can we do this later this week or next week?""",
-        'upsell': """Would you consider making this a monthy donation?"""}
+        'senddata': senddata,
+        'low28' : low28,
+        'funkybreak' : funkybreak}
 
-import sys
-import pyperclip
+
 
 if len(sys.argv) < 2:
     print('Usage: python mclip.py [keyphrase ] - copy phrase text')
