0e61f5e82ab10cc436962c2b4ed202b9ded49686
jfolsom <cercops@gmail.com>
2020-02-14 18:25:41 -0500

    dictionary chapter edx 6.001x

--- temp.py
+++ temp.py
@@ -4,9 +4,8 @@ aTup = ('fred', 'ed', 0, 999, (1, 2))
 def oddTuples(aTup):
     newtup = ()
     for count, item in enumerate(aTup):
-        if item%2 == 0:
-            newtup = newtup + item
+        if count%2 == 0:
+            newtup = newtup + (item,)
     return newtup
     
-print(oddTuples(aTup))
 print(oddTuples(aTup))
